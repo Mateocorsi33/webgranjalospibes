d9b9d7dd44f75e5efc7ffe2fbecd8cee84c81544
Mateo <mateofrontend@gmail.com>
2023-06-28 21:25:10 -0300

    fondoqs

--- src/Components/QuienesSomos/index.jsx
+++ src/Components/QuienesSomos/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import fondo from "../../assest/fondoqs.png";
+
+const ImgBack = styled.img`
+    width: 100%;
+`
 
 const QuienesSomos = () => {
     return (
-        <div>
-            Hola Quienes somos
-        </div>
+        <>
+            <ImgBack src={fondo} />
+        </>
     )
 }
 
