fb11190ebfe3b1d5c49c6dea59a8b0532c3c2a32
Mateo <mateofrontend@gmail.com>
2023-06-30 11:57:28 -0300

    form

--- src/Components/Donar/index.jsx
+++ src/Components/Donar/index.jsx
@@ -1,133 +1,173 @@
 import React from 'react';
 import styled from 'styled-components';
+import imagenlogo from "../../assest/logoglp.png";
+import fotoform from "../../assest/fotoform.png";
 
-const Form = styled.form`
+const FondoForm = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: #F5F5F5;
+  padding: 6% 15%;
+`
+
+const DivForm = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 70%;
+  height: auto;
+  border-radius: 10px;
+  background: #FFF;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`
+
+const Encabezado = styled.div`
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  gap: .8rem;
   width: 100%;
-  margin: 4rem 0;
+  height: 98px;
+  border-radius: 10px 10px 0px 0px;
+  background: #D6D582;
+`
+
+const ImgLogo = styled.img`
+    width: 3rem;
+    height: 3rem;
+    border-radius: 100%;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.6);
+
+    @media (max-width: 800px) {
+        margin-left: 1rem;
+    }
+`
+
+const TextLogo = styled.h2`
+    display: flex;
+    flex-direction: column;
+    width: 5rem;
+    font-weight: 500;
+    margin:0;
+    font-size: 1rem;
+    color: #ffffff;
+`
+
+const ImgForm = styled.img`
+  width: 100%;
+  height: 20rem;
+`
+
+const Titulo = styled.h1`
+    color: #D6D582;
+    text-align: center;
+    margin-top: 2rem;
+    font-size: 2rem;
+    font-family: Manrope;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    letter-spacing: 0.9px;
+
+    @media (max-width: 800px) {
+        display: flex;
+        font-size: 2rem;
+        text-align: center;
+        align-items: center;
+        font-weight: 500;
+    }
+`
+
+const Parrafo1 = styled.h2`
+  color: #464646;
+  text-align: center;
+  width: 80%;
+  font-size: 1.2rem;
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: 0.6px;
+`
+
+const Parrafo = styled.h3`
+  color: #464646;
+  text-align: center;
+  width: 80%;
+  font-size: 1rem;
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 0.54px;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 80%;
+  margin: 0;
 `;
 
 const Input = styled.input`
-  margin-bottom: 1rem;
-  padding: 0.5rem;
-  border-radius: 0.25rem;
-  border-color: #D6D582;
-  width: 80%;
-  height: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  width:18rem;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 5px;
 `;
 
 const Button = styled.button`
-    margin: 2.5rem 2rem;
-    background-color: #F48020;
-    border-radius: 10px;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    border: none;
-    width: 19rem;
-    height: 3rem;
-    font-size: 1rem;
-    font-weight: 400;
-    font-family: Manrope;
-    letter-spacing: 2.56px;
-    color: #ffffff;
-    cursor: pointer;
-    &:hover {
-        transition: .3s;
-        opacity: 70%;
-    }
-
-    @media (max-width: 800px) {
-        width: 90%;
-    }
+  margin-top: 20px;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  background-color: #0070f3;
+  color: #fff;
 `;
 
 function Donar() {
-  const [firstName, setFirstName] = React.useState('');
-  const [lastName, setLastName] = React.useState('');
-  const [donationType, setDonationType] = React.useState('one-time');
-  const [email, setEmail] = React.useState('');
-  const [cardNumber, setCardNumber] = React.useState('');
-  const [cardExpirationDate, setCardExpirationDate] = React.useState('');
-  const [cardSecurityCode, setCardSecurityCode] = React.useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log({
-      firstName,
-      lastName,
-      donationType,
-      email,
-      cardNumber,
-      cardExpirationDate,
-      cardSecurityCode,
-    });
-    // Aquí puedes enviar los datos del formulario a tu servidor
-    // usando fetch o axios.
-  }
-
   return (
-    <Form onSubmit={handleSubmit}>
-      <Input
-        type="text"
-        placeholder="Nombre"
-        value={firstName}
-        onChange={(event) => setFirstName(event.target.value)}
-      />
-      <Input
-        type="text"
-        placeholder="Apellido"
-        value={lastName}
-        onChange={(event) => setLastName(event.target.value)}
-      />
-      <label>
-        <input
-          type="radio"
-          name="donation-type"
-          value="one-time"
-          checked={donationType === 'one-time'}
-          onChange={(event) => setDonationType(event.target.value)}
-        />
-        Donación única vez
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="donation-type"
-          value="monthly"
-          checked={donationType === 'monthly'}
-          onChange={(event) => setDonationType(event.target.value)}
-        />
-        Donación mensual
-      </label>
-      <Input
-        type="email"
-        placeholder="Correo electrónico"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <Input
-        type="text"
-        placeholder="Número de tarjeta"
-        value={cardNumber}
-        onChange={(event) => setCardNumber(event.target.value)}
-      />
-      <Input
-        type="text"
-        placeholder="Fecha de vencimiento (MM/AA)"
-        value={cardExpirationDate}
-        onChange={(event) => setCardExpirationDate(event.target.value)}
-      />
-      <Input
-        type="text"
-        placeholder="Código de seguridad (CVV)"
-        value={cardSecurityCode}
-        onChange={(event) => setCardSecurityCode(event.target.value)}
-      />
-      <Button type="submit">Donar</Button>
-    </Form>
+    <FondoForm>
+      <DivForm>
+        <Encabezado>
+          <ImgLogo src={imagenlogo} alt="Logo"/>
+          <TextLogo>GRANJA LOS PIBES</TextLogo>
+        </Encabezado>
+        <ImgForm src={fotoform}/>
+        <Titulo>Dona hoy a Granja Los Pibes</Titulo>
+        <Parrafo1>Cada donación nos permite continuar nuestro trabajo y garantizar un futuro más prometedor para la fauna silvestre y los niños.</Parrafo1>
+        <Parrafo>Únete a nuestra misión y se parte del cambio positivo en la vida de los niños y la conservación de la fauna silvestre. Tu apoyo es fundamental para lograr un futuro más justo y sostenible. ¡Juntos podemos marcar la diferencia!</Parrafo>
+        <Parrafo1>¡Únete como padrino hoy mismo y sé parte del cambio!</Parrafo1>
+        <Form>
+          <h2>Información Personal</h2>
+          <Input type="text" placeholder="Nombre" />
+          <Input type="text" placeholder="Apellido" />
+          <Input type="email" placeholder="Correo Electrónico" />
+          <Input type="tel" placeholder="Teléfono" />
+          <Input type="text" placeholder="País" />
+          <Input type="text" placeholder="Provincia/Estado" />
+          <Input type="text" placeholder="Ciudad" />
+          <Input type="text" placeholder="Dirección" />
+          <Input type="text" placeholder="Código Postal" />
+
+          <h2>Información de Pago</h2>
+          <Input type="text" placeholder="Número de Tarjeta" />
+          <Input type="text" placeholder="Fecha de Expiración (MM/AA)" />
+          <Input type="text" placeholder="Código de Seguridad (CVV/CVC)" />
+          <Input type="text" placeholder="Número de Documento" />
+
+          <Button>Donar</Button>
+        </Form>
+      </DivForm>
+    </FondoForm>
   );
 }
 
 export default Donar;
+
+
