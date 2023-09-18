b0a5b0d6080fb3d0a55ccbe9f1c241b1df08510f
Mateo <mateofrontend@gmail.com>
2023-09-18 19:04:53 -0300

    sad

--- src/Components/Padrinos/fotos.jsx
+++ src/Components/Padrinos/fotos.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import Carousel from 'react-bootstrap/Carousel';
-import foto1 from "../../assest/p25.jpg";
+// import foto1 from "../../assest/p25.jpg";
 import foto2 from "../../assest/p1.jpg";
-import foto3 from "../../assest/p28.jpg";
+// import foto3 from "../../assest/p28.jpg";
 import foto4 from "../../assest/p3.jpg";
 import foto5 from "../../assest/p4.jpg";
 import foto6 from "../../assest/p5.jpg";
-import foto7 from "../../assest/p27.jpg";
-import foto8 from "../../assest/p24.jpg";
-import foto9 from "../../assest/p23.jpg";
+// import foto7 from "../../assest/p27.jpg";
+// import foto8 from "../../assest/p24.jpg";
+// import foto9 from "../../assest/p23.jpg";
 import foto10 from "../../assest/foto2.jpg";
 
 
@@ -41,18 +41,18 @@ const Img = styled.img`
 function CarouselImg() {
   return (
     <StyledCarousel interval={3000}>
-        <Carousel.Item>
+        {/* <Carousel.Item>
             <Img className="d-block w-100" src={foto1} alt="Granja"/>
             <Carousel.Caption><h3></h3><p></p></Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item> */}
         <Carousel.Item>
             <Img className="d-block w-100" src={foto2} alt="Second slide"/>
             <Carousel.Caption><h3></h3><p></p></Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        {/* <Carousel.Item>
             <Img className="d-block w-100" src={foto3} alt="Third slide"/>
             <Carousel.Caption><h3></h3><p></p></Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item> */}
         <Carousel.Item>
             <Img className="d-block w-100" src={foto4} alt=""/>
             <Carousel.Caption><h3></h3><p></p></Carousel.Caption>
@@ -65,7 +65,7 @@ function CarouselImg() {
             <Img className="d-block w-100" src={foto6} alt=""/>
             <Carousel.Caption><h3></h3><p></p></Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        {/* <Carousel.Item>
             <Img className="d-block w-100" src={foto7} alt=""/>
             <Carousel.Caption><h3></h3><p></p></Carousel.Caption>
         </Carousel.Item>
@@ -76,7 +76,7 @@ function CarouselImg() {
         <Carousel.Item>
             <Img className="d-block w-100" src={foto9} alt=""/>
             <Carousel.Caption><h3></h3><p></p></Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item> */}
         <Carousel.Item>
             <Img className="d-block w-100" src={foto10} alt=""/>
             <Carousel.Caption><h3></h3><p></p></Carousel.Caption>
